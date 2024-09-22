00af42cc73d8eddfdccee9b922f8560da14760de
Hungi03 <hunghung2k33@gmail.com>
2024-09-22 18:04:34 +0700

    Add UserDTO

---
+++ DTO/ListScreenDTO.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    public class ListScreenDTO
+    {
+        public string ScreenID { get; set; }
+        public string? ScreenName { get; set; }
+        public ListScreenDTO()
+        {
+            ScreenID = GenerateScreenID();
+        }
+        public ListScreenDTO(string? screenName)
+        {
+            ScreenID = GenerateScreenID();
+            ScreenName = screenName;
+        }
+        private string GenerateScreenID()
+        {
+            return "SCR" + Guid.NewGuid().ToString("N");
+        }
+    }
+}
--- GUI/Program.cs
+++ GUI/Program.cs
@@ -13,7 +13,7 @@ namespace GUI
         /// </summary>
         [STAThread]
         static void Main()
-        {
+       {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
