413693ea3432ce8da6ada6a6a5a9b6fb9171db4a
Trần Văn Đảo <vandaopq@gmail.com>
2024-09-22 17:58:41 +0700

    add OrdersDTO, PurchaseReceiptDetailsDTO, PurchaseReceiptsDTO, SupliersDTO

---
+++ DTO/PurchaseReceiptDetailsDTO.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    public class PurchaseReceiptDetailsDTO
+    {
+        public string? PReceiptID { get; set; }
+        public string? ProductID { get; set; }
+        public int Quantity { get; set; } = 0;
+        public double? Price { get; set; }
+        public PurchaseReceiptDetailsDTO() { }
+        public PurchaseReceiptDetailsDTO(string? pReceiptID, string? productID, int quantity, double? price)
+        {
+            PReceiptID = pReceiptID;
+            ProductID = productID;
+            Quantity = quantity;
+            Price = price;
+        }
+    }
+}
